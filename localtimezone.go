139216764488d1c01e4f24c226d52f42b2309db9
Albert Wang <git@albertyw.com>
2022-09-07 21:48:19 -0700

    Fix typo

--- localtimezone.go
+++ localtimezone.go
@@ -14,7 +14,7 @@
 //
 // * The shapefile is simplified using a lossy method so it may be innacurate along the borders
 //
-// * This is purerly in-memory. Uses ~50MB of ram
+// * This is purely in-memory. Uses ~50MB of ram
 package localtimezone
 
 import (
