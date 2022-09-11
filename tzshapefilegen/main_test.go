442136101b56e0bc3a33d8d10e2d523a054aa8d1
Albert Wang <git@albertyw.com>
2022-09-10 22:23:21 -0700

    Add test to check if data is up to date

---
+++ tzshapefilegen/main_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/albertyw/localtimezone"
+)
+
+func TestGetMostCurrentRelease(t *testing.T) {
+	version, url, err := getMostCurrentRelease()
+	if err != nil {
+		t.Errorf("cannot get most current timezone boundary")
+	}
+	if url == "" {
+		t.Errorf("cannot get most current timezone url")
+	}
+	if version != localtimezone.TZBoundaryVersion {
+		t.Errorf("timezone boundary is out of date")
+	}
+}
