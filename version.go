e89c88e103c63f7d387524562e325e93b1a99f29
Albert Wang <git@albertyw.com>
2026-01-08 00:09:10 -0800

    Update to tzdata 2025c

--- version.go
+++ version.go
@@ -3,7 +3,7 @@
 package localtimezone
 
 // TZBoundaryVersion is the version of tzdata that was used to generate timezone boundaries
-const TZBoundaryVersion = "2025b"
+const TZBoundaryVersion = "2025c"
 
 // TZCount is the number of tzdata timezones supported
 const TZCount = 444
