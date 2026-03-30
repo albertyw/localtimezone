976166d4edb4aac78e2e48484905a4d6d04ae810
Albert Wang <git@albertyw.com>
2026-03-29 20:09:48 -0700

    Include mock data generation in go generate

--- gen.go
+++ gen.go
@@ -1,3 +1,4 @@
 //go:generate go run -modfile=tzshapefilegen/go.mod tzshapefilegen/main.go
+//go:generate go run -modfile=tzshapefilegen/go.mod tzshapefilegen/genmock/main.go
 
 package localtimezone
